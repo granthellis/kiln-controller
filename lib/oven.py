d0d458c500474f53cd9bf8108b7bf024ae459d1e
Grant Ellis <grant.h.ellis@gmail.com>
2024-03-30 19:34:01 +1100

    Add logic for peak/offpeak rates to get_state()

--- lib/oven.py
+++ lib/oven.py
@@ -328,7 +328,7 @@ class Oven(threading.Thread):
             # this happens at start-up with a simulated oven
             temp = 0
             pass
-
+            rate = config.kwh_rate_peak if is_peak() else config.kwh_rate
         state = {
             'cost': self.cost,
             'runtime': self.runtime,
@@ -337,7 +337,7 @@ class Oven(threading.Thread):
             'state': self.state,
             'heat': self.heat,
             'totaltime': self.totaltime,
-            'kwh_rate': config.kwh_rate,
+            'kwh_rate': rate,
             'currency_type': config.currency_type,
             'profile': self.profile.name if self.profile else None,
             'pidstats': self.pid.pidstats,
